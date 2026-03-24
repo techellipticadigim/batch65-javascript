74f36a41c4e603c9cbc236161c0890d9281ec1a7
Vaibhav <vaibhavraghuvanshi58@gmail.com>
2026-03-24 20:37:38 +0530

    Initial commit

---
+++ day1.md
@@ -0,0 +1,28 @@
+
+
+npm init
+
+
+javascript ? - web designing
+HTML , CSS, javascipt
+
+
+nodejs
+npm - node package manager
+
+
+github desktop
+
+term - 
+
+github portal - https://www.github.com. - REMOTE REPOSITORY
+
+BATCH65-JAVASCIPT - WORKING REPOSITORY
+
+Home work 
+
+create a javascript project (home work) and send to remote repository
+send me screenshot
+
+
+
---
+++ js/test.js
@@ -0,0 +1,22 @@
+
+// variable ? - vary + able
+// use to store information
+// oppsite of variable -> constant 
+
+myname = "Pooja";
+age = 40;
+city = "mumbai"
+
+//console.log("my name is ", myname , "." ,"my age is ",age, "i live in ", city); 
+console.log(`my name is ${myname}. my age is  ${age} i live in  ${city}`);
+
+// bactick
+// console.log("My name is vaibhav singh.");
+// console.log("my age is 34.");
+// console.log("i live in pune");
+
+// how to execute my code
+// change direction - cd
+// 1 console.log. = 1 line
+
+
